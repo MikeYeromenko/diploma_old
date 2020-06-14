0d5102e1effdb9db80fb56b0c818fe40351774c3
Mike <inboxema2015@gmail.com>
2020-06-14 13:23:56 +0300

    Refactors test_models.py, adds full testing for models

--- seance/models.py
+++ seance/models.py
@@ -8,6 +8,12 @@ from django.utils.translation import gettext_lazy as _
 
 class AdvUser(AbstractUser):
     wallet = models.FloatField(blank=True, null=True, verbose_name=_('wallet'))
+    was_deleted = models.BooleanField(default=False, verbose_name=_('was deleted?'))
+
+    def delete(self, *args, **kwargs):
+        self.is_active = False
+        self.was_deleted = True
+        self.save()
 
 
 class Film(models.Model):
@@ -16,36 +22,36 @@ class Film(models.Model):
     director = models.CharField(max_length=100, verbose_name=_('director'))
     duration = models.TimeField(verbose_name=_('duration'))
     description = models.TextField(verbose_name=_('description'))
-    created = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_('Instance created at'))
-    updated = models.DateTimeField(auto_now=True, verbose_name=_('Instance updated at'))
-    admin = models.ForeignKey(AdvUser, on_delete=models.DO_NOTHING, verbose_name=_('Instance created by'))
-    is_active = models.BooleanField(default=True, verbose_name=_('In run?'))
+    created = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_('instance created at'))
+    updated = models.DateTimeField(auto_now=True, verbose_name=_('instance updated at'))
+    admin = models.ForeignKey(AdvUser, on_delete=models.PROTECT, verbose_name=_('instance created by'))
+    is_active = models.BooleanField(default=True, verbose_name=_('in run?'))
 
 
 class Hall(models.Model):
     name = models.CharField(max_length=20, verbose_name=_('name'))
-    size = models.PositiveIntegerField(verbose_name=_('How many seats?'))
+    size = models.PositiveIntegerField(verbose_name=_('how many seats?'))
     description = models.TextField(verbose_name=_('description'))
-    created = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_('Instance created at'))
-    updated = models.DateTimeField(auto_now=True, verbose_name=_('Instance updated at'))
-    admin = models.ForeignKey(AdvUser, on_delete=models.DO_NOTHING, verbose_name=_('Instance created by'),
+    created = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_('instance created at'))
+    updated = models.DateTimeField(auto_now=True, verbose_name=_('instance updated at'))
+    admin = models.ForeignKey(AdvUser, on_delete=models.PROTECT, verbose_name=_('instance created by'),
                               related_name='halls')
-    is_active = models.BooleanField(default=True, verbose_name=_('In run?'))
+    is_active = models.BooleanField(default=True, verbose_name=_('in run?'))
 
 
 class Seance(models.Model):
     film = models.ForeignKey(Film, on_delete=models.PROTECT, related_name='seances', verbose_name=_('films'))
     date_starts = models.DateField(verbose_name=_('starts'))
-    date_ends = models.DateField(verbose_name=_('ends'))
+    date_ends = models.DateField(null=True, blank=True, verbose_name=_('ends'))
     time_starts = models.TimeField(verbose_name=_('starts at: '))
-    time_ends = models.TimeField(verbose_name=_('ends at: '))
+    time_ends = models.TimeField(null=True, blank=True, verbose_name=_('ends at: '))
     places_taken = models.PositiveIntegerField(default=0, verbose_name=_('places taken'))
     hall = models.ForeignKey(Hall, on_delete=models.PROTECT, related_name='seances', verbose_name=_('hall'))
     description = models.TextField(verbose_name=_('description'))
-    created = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_('Instance created at'))
-    updated = models.DateTimeField(auto_now=True, verbose_name=_('Instance updated at'))
-    admin = models.ForeignKey(AdvUser, on_delete=models.DO_NOTHING, verbose_name=_('Instance created by'))
-    is_active = models.BooleanField(default=True, verbose_name=_('In run?'))
+    created = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_('instance created at'))
+    updated = models.DateTimeField(auto_now=True, verbose_name=_('instance updated at'))
+    admin = models.ForeignKey(AdvUser, on_delete=models.PROTECT, verbose_name=_('instance created by'))
+    is_active = models.BooleanField(default=True, verbose_name=_('in run?'))
     ticket_price = models.DecimalField(max_digits=15, decimal_places=2, verbose_name=_('ticket costs:'))
 
     def save(self, *args, **kwargs):
@@ -67,14 +73,17 @@ class Seance(models.Model):
         """
         Sets time_ends field of seance, if it was noy set by admin, adding to time_starts the film's
         duration and 10 minutes for advertisements.
-        datetime.datetime(...) is used for us to get readable time value, for example if time_starts is 23 hours,
+        Code here is used for us to get a readable time value, for example if time_starts is 23 hours,
         and film.duration is 2 hours, we'll set time_ends at 1 hour 10 minutes, but not at 25 hours 10 minutes
         """
-        hours = self.time_starts.hour + self.film.duration.hour
         minutes = self.time_starts.minute + self.film.duration.minute + 10
-        time_ends = datetime.datetime(2020, 1, 1, 0, 0, 0, 0) + datetime.timedelta(hours=hours,
-                                                                                   minutes=minutes)
-        self.time_ends = datetime.time(hour=time_ends.hour, minute=time_ends.minute)
+
+        # if value of minutes is more then 60, add integer part of it to hours
+        hours = (self.time_starts.hour + self.film.duration.hour + minutes // 60) % 24
+
+        # fractional part will be less then 60, so we leave it as minutes value
+        minutes %= 60
+        self.time_ends = datetime.time(hour=hours, minute=minutes)
 
 
 class Ticket(models.Model):
--- seance/tests/test_models.py
+++ seance/tests/test_models.py
@@ -1,9 +1,10 @@
 import datetime
 from django.contrib.auth import get_user_model
+from django.db.models import ProtectedError
 from django.test import TestCase
 from django.utils import timezone
 
-from seance.models import Film, Hall, Seance
+from seance.models import Film, Hall, Seance, AdvUser
 
 
 class BaseInitial:
@@ -55,8 +56,15 @@ class SeanceModelsTestCase(TestCase, BaseInitial):
         Tests that Film object was created correctly
         """
         self.assertEqual(self.film.title, 'James Bond')
+        self.assertEqual(self.film.starring, 'Daniel Craig and co')
+        self.assertEqual(self.film.director, 'Director')
+        self.assertEqual(self.film.description, 'some text....')
         self.assertEqual(self.film.duration, datetime.time(1, 40))
         self.assertEqual(self.film.admin.username, 'admin')
+        self.assertTrue(self.film.is_active)
+
+        # film was created no more then 2 minutes ago
+        self.assertTrue(timezone.now() - datetime.timedelta(minutes=2) < self.film.created < timezone.now())
 
     def test_hall_model_basic(self):
         """
@@ -65,15 +73,30 @@ class SeanceModelsTestCase(TestCase, BaseInitial):
         self.assertEqual(self.hall.name, 'Yellow')
         self.assertEqual(self.hall.size, 50)
         self.assertEqual(self.hall.admin.username, 'admin')
+        self.assertEqual(self.hall.description, 'Some text about why this hall is the best')
+        self.assertTrue(self.hall.is_active)
+
+        # hall was created no more then 2 minutes ago
+        self.assertTrue(timezone.now() - datetime.timedelta(minutes=2) < self.hall.created < timezone.now())
 
     def test_seance_model_basic(self):
         """
         Tests that Seance object was created correctly
         """
         self.assertEqual(self.seance.film.title, 'James Bond')
+        self.assertEqual(self.seance.date_starts, datetime.date.today())
+        self.assertEqual(self.seance.date_ends, datetime.date.today() + datetime.timedelta(days=15))
+        self.assertEqual(self.seance.time_starts, datetime.time(12))
         self.assertEqual(self.seance.ticket_price, 100)
         self.assertEqual(self.seance.time_ends, datetime.time(13, 50))
         self.assertEqual(self.seance.hall.name, 'Yellow')
+        self.assertEqual(self.seance.places_taken, 0)
+        self.assertEqual(self.seance.description, 'Some text about why this seance is the best')
+        self.assertTrue(self.seance.is_active)
+        self.assertEqual(self.seance.admin.username, 'admin')
+
+        # seance was created no more then 2 minutes ago
+        self.assertTrue(timezone.now() - datetime.timedelta(minutes=2) < self.seance.created < timezone.now())
 
     def test_default_seance_end_date_auto_added(self):
         """
@@ -97,7 +120,9 @@ class SeanceModelsTestCase(TestCase, BaseInitial):
         """
         Test the seance time_ends to be set correctly
         """
-        seance = Seance.objects.create(
+        # create new seance object based on self.seance with new time_starts. time_ends will be
+        # 24 hours and 50 minutes (+10min for advertisement). Here we check it will become 0 hour 50 minutes
+        seance1 = Seance.objects.create(
             film=self.film,
             date_starts=datetime.date.today(),
             date_ends=datetime.date.today() + datetime.timedelta(days=30),
@@ -109,8 +134,10 @@ class SeanceModelsTestCase(TestCase, BaseInitial):
             ticket_price=100,
             admin=self.admin,
         )
-        self.assertEqual(seance.time_ends, datetime.time(0, 50))
+        self.assertEqual(seance1.time_ends, datetime.time(0, 50))
 
+        # create new seance object based on self.seance with new time_starts. time_ends will be
+        # 24 hours and 100 minutes (+10min for advertisement). Here we check it will become 1 hour 40 minutes
         seance2 = Seance.objects.create(
             film=self.film,
             date_starts=datetime.date.today(),
@@ -128,9 +155,83 @@ class SeanceModelsTestCase(TestCase, BaseInitial):
         #   Test if in Yellow hall there are 3 seances
         self.assertEqual(Seance.objects.filter(hall=self.hall).count(), 3)
 
-    def test_user_created(self):
+    def test_admin_created(self):
         """
         Test if user was created, with initial data
         """
         self.assertEqual(self.admin.username, 'admin')
         self.assertTrue(self.admin.is_superuser)
+        self.assertTrue(self.admin.is_staff)
+        #   Test user's type class has name AdvUser
+        self.assertEqual(type(self.admin).__name__, 'AdvUser')
+
+    def test_admin_cannot_be_deleted(self):
+        """
+        We can't delete admin. If we del, admin.is_active becomes False, but instance is left at database
+        """
+        admin2 = AdvUser.objects.create_superuser(username='admin2', email='admin2@some_site.com')
+        admin2.set_password('password2')
+        admin2.save()
+        self.assertTrue(admin2.is_active)
+        admin2.delete()
+        self.assertFalse(admin2.is_active)
+        self.assertTrue(admin2.was_deleted)
+        self.assertTrue(AdvUser.objects.filter(username='admin2'))
+
+    def test_user_created_and_deleted(self):
+        """
+        Test that user is created correctly
+        """
+        user = AdvUser.objects.create(
+            username='user1',
+            email='user1@somesite.com',
+            first_name='Mike',
+            last_name='Yeromenko',
+            wallet=10000,
+        )
+        self.assertEqual(user.username, 'user1')
+        self.assertEqual(user.email, 'user1@somesite.com')
+        self.assertEqual(user.first_name, 'Mike')
+        self.assertEqual(user.last_name, 'Yeromenko')
+        self.assertEqual(user.wallet, 10000.00)
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+        self.assertTrue(user.is_active)
+
+        # user was created no more then one minute ago
+        self.assertTrue(timezone.now() - datetime.timedelta(minutes=1) < user.date_joined < timezone.now())
+
+        # inbuilt methods give what we expect
+        self.assertEqual(user.get_full_name(), 'Mike Yeromenko')
+        self.assertEqual(user.get_short_name(), 'Mike')
+
+        # after deletion of user it's not deleted, but just gets attribute is_active=False
+        # and was_deleted=True
+        user.delete()
+        self.assertTrue(user.was_deleted)
+        self.assertFalse(user.is_active)
+
+    def test_instances_can_be_deleted(self):
+        """
+        Test if we can delete Seance, Hall, Film and AdvUser objects object
+        """
+        # We can't delete Film object, if there are seance references on it
+        with self.assertRaises(ProtectedError):
+            Film.objects.last().delete()
+
+        # We can't delete Hall object, if there are seance references on it
+        with self.assertRaises(ProtectedError):
+            Hall.objects.last().delete()
+
+        # delete seance object, but before watch it exists
+        self.assertEqual(Seance.objects.all().count(), 1)
+        Seance.objects.last().delete()
+        self.assertEqual(Seance.objects.all().count(), 0)
+
+        # after that Film and Hall objects can be deleted
+        Film.objects.last().delete()
+        Hall.objects.last().delete()
+
+        self.assertEqual(Film.objects.all().count(), 0)
+        self.assertEqual(Hall.objects.all().count(), 0)
+
