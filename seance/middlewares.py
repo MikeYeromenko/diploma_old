b60192c2ead39241cb215a6d0038380fc25911d8
Mike <inboxema2015@gmail.com>
2020-06-15 10:03:10 +0300

    Adds LogoutIfNotActiveMiddleware to logout user if more then 5 minutes inactive

--- seance/middlewares.py
+++ seance/middlewares.py
@@ -1,15 +1,23 @@
+from django.contrib.auth import logout
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
 from django.utils.deprecation import MiddlewareMixin
 
 from cinema import settings
+from seance.models import AdvUser
 
 
-class LogoutIfNotActiveMiddleware(MiddlewareMixin):
+class LogoutIfInActiveMiddleware(MiddlewareMixin):
     def process_request(self, request):
         assert hasattr(request, 'user'), (
             'The LogoutIfNotActiveMiddleware middleware requires authentication middleware to be installed.'
         )
         if request.user.is_authenticated:
-            pass
+            if request.user.last_activity > timezone.now() - timezone.timedelta(minutes=5):
+                user = get_object_or_404(AdvUser, pk=request.user.pk)
+                user.last_activity = timezone.now()
+                user.save()
+                request.user.last_activity = user.last_activity
+            else:
+                logout(request)
 
-
-        request.user = SimpleLazyObject(lambda: get_user(request))
